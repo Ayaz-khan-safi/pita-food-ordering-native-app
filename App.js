8875bb573344a305619bc23ffc859404f087388d
ayaz.khan <ayaz.khan@ceative.co.uk>
2023-10-17 23:15:30 +0500

    new order details page added

--- App.js
+++ App.js
@@ -15,7 +15,7 @@ export default function App() {
     <NavigationContainer>
       <StatusBar hidden />
       <Stack.Navigator
-        initialRouteName="dashboard"
+        initialRouteName="orderDetails"
         screenOptions={{
           header: (props) => <AppHeader {...props} />,
         }}
--- screens/orderDetails.js
+++ screens/orderDetails.js
@@ -1,26 +1,55 @@
 import React from "react";
-import { View, Text, Image, StyleSheet } from "react-native";
+import { View, Text, Image, StyleSheet, TouchableOpacity } from "react-native";
 
 const OrderDetailsScreen = () => {
+  const handleAssignRider = () => {
+    // Logic to assign a rider to the order
+    console.log("Rider assigned!");
+  };
+
+  const handleCancelOrder = () => {
+    // Logic to cancel the order
+    console.log("Order canceled!");
+  };
   return (
     <View style={styles.container}>
+
+        <View style={styles.statusContainer}>
+          <Text style={styles.status}>Delivery Pending</Text>
+        </View>
+        <View style={styles.orderIdContainer}>
+        <Text style={styles.orderId}>Order ID: 14</Text>
+        </View>
       <View style={styles.orderInfoContainer}>
-        <Text style={styles.orderId}>Order ID: </Text>
-        <Text style={styles.type}>Type: </Text>
-        <Text style={styles.status}>Status: </Text>
+        
       </View>
       <View style={styles.customerInfoContainer}>
-        <Text style={styles.customerName}>Customer: </Text>
-        <Text style={styles.address}>Address: </Text>
+        <Text style={styles.customerName}>Customer: John Doe</Text>
+        <Text style={styles.address}>Address: 6th st, North ave, Halway Rd, London, United Kingdom</Text>
+        <Text style={styles.price}>Price: $14.99</Text>
+        <Text style={styles.quantity}>Quantity: 2</Text>
       </View>
       <View style={styles.orderDetailsContainer}>
-        <Text style={styles.quantity}>Quantity: </Text>
-        <Text style={styles.price}>Price: $</Text>
+        <Text style={styles.type}>Type: Pizza</Text>
+        <Text style={styles.TotalPrice}>Total: $29.98</Text>
       </View>
       <Image
         source={require("../assets/OrderCardImage/pizzaImage.png")}
         style={styles.image}
       />
+      <View style={styles.buttonContainer}>
+        {/* Cancel Order Button */}
+        <TouchableOpacity style={[styles.button, { backgroundColor: 'red' }]} onPress={handleCancelOrder}>
+          <Text style={styles.buttonText}>Cancel Order</Text>
+        </TouchableOpacity>
+
+        {/* Assign Rider Button */}
+        <TouchableOpacity style={[styles.button, { backgroundColor: 'green' }]} onPress={handleAssignRider}>
+          <Text style={styles.buttonText}>Assign To a Rider</Text>
+        </TouchableOpacity>
+      </View>
+
+
     </View>
   );
 };
@@ -33,17 +62,35 @@ const styles = StyleSheet.create({
     padding: 20,
   },
   orderInfoContainer: {
+    flexDirection: "row",
+    justifyContent: 'space-between',
+    paddingHorizontal: 20,
     marginBottom: 20,
   },
+  orderIdContainer : {
+    position: "absolute",
+    top: 50,
+    left: 10,  },
   orderId: {
     fontSize: 18,
     fontWeight: "bold",
+
   },
   type: {
-    fontSize: 16,
+    fontSize: 24,
+    fontWeight: "bold",
+    textTransform: "uppercase",
+  },
+  statusContainer : {
+    position: "absolute",
+    top: 50,
+    right: 10,
   },
   status: {
-    fontSize: 16,
+    fontSize: 18,
+    fontWeight: "bold",
+    textTransform: "uppercase",
+    color: "red"
   },
   customerInfoContainer: {
     marginBottom: 20,
@@ -63,10 +110,34 @@ const styles = StyleSheet.create({
   price: {
     fontSize: 16,
   },
+  TotalPrice : {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 5,
+  },
   image: {
-    width: 200,
-    height: 200,
+    width: '100%',
+    height: 250,
     resizeMode: "cover",
+    marginBottom: 20,
+  },
+
+  buttonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: '100%',
+    paddingHorizontal: 20,
+    marginTop: 20,
+  },
+  button: {
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 5,
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 16,
+    textAlign: 'center',
   },
 });
 
