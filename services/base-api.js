28c469e68a1108f4967108e3e375fd58211438a9
ayaz.khan <ayaz.khan@ceative.co.uk>
2023-12-05 20:24:06 +0500

    new changes

--- services/base-api.js
+++ services/base-api.js
@@ -5,7 +5,7 @@ import { EXPO_DEFAULT_BASE_URL } from "@env";
 
 const baseQuery = fetchBaseQuery({
   // baseUrl: EXPO_DEFAULT_BASE_URL,
-  baseUrl: "https://6e34-116-71-184-59.ngrok.io",
+  baseUrl: "https://5fb8-116-71-10-84.ngrok.io",
   prepareHeaders: async (headers) => {
     const token = await SecureStore.getItemAsync("token");
     if (token) {
